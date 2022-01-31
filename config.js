a2d32ce3412a0a473e157fbe0a56faf23ed77397
Soumya Talwar <soumya.talwar97@gmail.com>
2022-01-31 06:46:23 +0530

    hide api keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'aRZEEIQSzuNtrLxEuwVmtXFDZ',
-  consumer_secret:      'qSXfnhwUw6B18HxFjiiQ6f17WULjNSTTKmlWLNLb16qmlOhpky',
-  access_token:         '1318158170577989637-DUSgsRcLfyU09HGiZpsEReWqhFXb3h',
-  access_token_secret:  'cVuoJLHx6o0GiUXBJ1XzKTn78O77mdtzVLCyXz5PL5cRj'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
--- config2.js
+++ config2.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyBrEUnl_i9nm3HsxCS1y8MBc738zBnAgow",
-  authDomain: "taegi-fic-bot.firebaseapp.com",
-  projectId: "taegi-fic-bot",
-  databaseURL: "https://taegi-fic-bot-default-rtdb.firebaseio.com/",
-  storageBucket: "taegi-fic-bot.appspot.com",
-  messagingSenderId: "757928575661",
-  appId: "1:757928575661:web:4620281a5eb6ff9c053c24",
-  measurementId: "G-23V63BL07X"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 }
